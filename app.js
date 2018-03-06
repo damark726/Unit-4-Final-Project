e7457583a9d50d774c496e60957d07c4ffc53968
David Markoc <davidmarkoc@Davids-MacBook-Pro.local>
2018-03-06 11:35:40 -0500

    deployment6

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 3001;
 app.use(morgan("dev"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(express.static("client"));
-app.use(express.static(path.join(__dirname, "client/public")))
+app.use(express.static("client/build"));
+// app.use(express.static(path.join(__dirname, "client/build")))
 app.use(cookieParser());
 app.use(session({secret: process.env.SESSION_KEY, resave: false, saveUninitialized: true}));
 app.use(passport.initialize());
